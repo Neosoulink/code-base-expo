42e6a76407c11bd6cb57ecb3d1d84036d6d39274
Neosoulink <neodarksoulink@gmail.com>
2022-01-19 16:03:05 +0100

    Add config To get smaller bundle size by excluding modules

--- babel.config.ts
+++ babel.config.ts
@@ -2,5 +2,10 @@ module.exports = function (api: any) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
+		env: {
+			production: {
+				plugins: ["react-native-paper/babel"],
+			},
+		},
 	};
 };
