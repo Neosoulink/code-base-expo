2202e29350fa3cb15ac39c9aada2ba0f483a7da2
Neosoulink <neodarksoulink@gmail.com>
2022-01-19 20:11:42 +0100

    Link app & fix Loading page

--- src/screens/Loading.screen.tsx
+++ src/screens/Loading.screen.tsx
@@ -1,55 +1,12 @@
-import React from 'react';
-import { View, Animated, Easing, StyleSheet } from 'react-native';
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
 
 // STYLES
-import {
-	ConstantColor as CC,
-	GlobalStyle as GS
-} from '../assets/js/style';
-import FocusAwareStatusBar from '../components/FocusAwareStatusBar';
+import { GLOBAL_STYLE as GS } from "../assets/ts/styles";
+import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
 
-export default ({ }) => {
+export default ({}) => {
 	// STATES
-	const [animation, setAnimation] = React.useState(new Animated.Value(0));
-
-	// DATA
-	const interpolated = animation.interpolate({
-		inputRange: [0, 1],
-		outputRange: [0.3, 1],
-	});
-
-	const animatedStyle = {
-		...GS.w100,
-		...GS.h100,
-		opacity: interpolated,
-	};
-
-	// EFFECTS
-	React.useEffect(() => {
-		Animated.loop(
-			Animated.sequence([
-				Animated.timing(
-					animation,
-					{
-						toValue: 1,
-						duration: (1200),
-						easing: Easing.linear(),
-						useNativeDriver: false,
-					}
-				),
-				Animated.timing(
-					animation,
-					{
-						toValue: 0,
-						duration: (1200),
-						easing: Easing.linear(),
-						useNativeDriver: false,
-					}
-				)
-			])
-		).start();
-		return () => { }
-	}, [])
 
 	return (
 		<View style={{ ...GS.screen, ...GS.centered }}>
@@ -59,38 +16,7 @@ export default ({ }) => {
 				backgroundColor="transparent"
 			/>
 
-			<View style={styles.main} >
-				<Animated.Image
-					source={require("../assets/img/appIcons/splash.png")}
-					resizeMode="contain"
-					style={animatedStyle}
-				/>
-			</View>
-
+			<ActivityIndicator size="large" />
 		</View>
 	);
-}
-
-const styles = StyleSheet.create({
-	main: {
-		...GS.screen,
-		...GS.centered,
-		...GS.bgPrimary,
-		...GS.w100,
-		...GS.h100,
-	},
-	titleLogo: {
-		...GS.pt5,
-		fontSize: 60,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		color: CC.light
-	},
-	subTitleLogo: {
-		fontSize: 18,
-		textAlign: 'center',
-		color: CC.light,
-		lineHeight: 40
-	}
-})
-
+};
