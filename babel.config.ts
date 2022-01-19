563d3c15addd335593267afe02a9bcef42eaddce
Neosoulink <neodarksoulink@gmail.com>
2022-01-19 18:38:02 +0100

    Minor fix

--- babel.config.ts
+++ babel.config.ts
@@ -2,10 +2,5 @@ module.exports = function (api: any) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
-		env: {
-			production: {
-				plugins: ["react-native-paper/babel"],
-			},
-		},
 	};
 };
