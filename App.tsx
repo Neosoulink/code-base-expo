ffedc7d6c18ca9a3f8c239437bfefec8bab84e12
Neosoulink <neodarksoulink@gmail.com>
2022-01-22 19:55:13 +0100

    fix: temporary testing fix (TODO: implement jsx testing and E2E tests)

--- App.tsx
+++ App.tsx
@@ -1,3 +1,12 @@
-import App from "./src/App";
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+	return (
+		<View>
+			<Text>Hey</Text>
+		</View>
+	);
+};
 
 export default App;
