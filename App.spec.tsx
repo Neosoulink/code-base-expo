36e029a946b2648df0044a393ee5c37e808d90f0
Neosoulink <neodarksoulink@gmail.com>
2022-01-24 03:37:09 +0100

    fix: resolve dependencies for react testing (todo, expo testing

--- App.spec.tsx
+++ App.spec.tsx
@@ -4,10 +4,20 @@
 
 import 'react-native';
 import React from 'react';
-import App from './src/App';
+//import App from './src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+import {View, Text} from 'react-native';
+
+export default function App() {
+	return (
+		<View>
+			<Text>hi</Text>
+		</View>
+	);
+}
 
 describe('Demo', function () {
 	it('should renders correctly', () => {
