1192f0fd746c18f0d8e4637cefd1b0d6e9b71e9b
Neosoulink <neodarksoulink@gmail.com>
2023-05-06 20:56:55 +0300

    chore(config): setup path alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,35 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					extensions: [
+						'.ios.js',
+						'.android.js',
+						'.ios.jsx',
+						'.android.jsx',
+						'.js',
+						'.jsx',
+						'.json',
+						'.ts',
+						'.tsx',
+					],
+					root: ['.'],
+					alias: {
+						'~assets': './src/assets',
+						'~components': './src/components',
+						'~fonts': './src/assets/fonts',
+						'~hooks': './src/hooks',
+						'~helpers': './src/helpers',
+						'~imgs': './src/assets/img',
+						'~screens': ['./src/screens'],
+						'~store': './src/store',
+						'~styles': './src/assets/ts/styles',
+					},
+				},
+			],
+		],
 	};
 };
